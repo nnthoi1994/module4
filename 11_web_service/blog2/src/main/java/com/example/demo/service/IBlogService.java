2d1dce42845ac5094fda7cae9031b5fdb57bffe8
nnthoi1994 <nnthoi1994@gmail.com>
2025-11-10 16:10:11 +0700

    first commit

---
+++ 11_web_service/blog2/src/main/java/com/example/demo/service/IBlogService.java
@@ -0,0 +1,19 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Blog;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IBlogService{
+    List<Blog> findAll();
+
+    void save(Blog blog);
+
+    Blog findById(Integer id);
+    void deleteById(Integer id);
+    List<Blog> findByCategoryId(Integer categoryId);
+    List<Blog> findByTitleContaining(String keyword);
+    Page<Blog> searchByTitleAndCategory(String title, Integer categoryId, Pageable pageable);
+}
